97339575ac5cc39d9b529f147a6afffefffb190e
HummusPrince <HummusPrince@dev.null>
2022-01-25 10:21:11 +0200

    Can get args

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("hello world!");
+int main(int argc, char **argv){
+    printf("Got %d args\n", argc);
+    for(int i = 0; i < argc; i++) printf("arg[%d] = %s\n", i, argv[i]);
     return 0;
 }
