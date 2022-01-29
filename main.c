5aaf117598f189d14fca5669193e915cac775f4f
HummusPrince <HummusPrince@dev.null>
2022-01-30 00:36:22 +0200

    Can properly write a file

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <errno.h>
+//#include <unistd.h>
 
+//Call - at89prog <serial device> <hex file>
+//For now only device read is supported...
 int main(int argc, char **argv){
-    printf("Got %d args\n", argc);
-    for(int i = 0; i < argc; i++) printf("arg[%d] = %s\n", i, argv[i]);
+    if(argc != 3){
+        fprintf(stderr, "Usage - at89prog <serial device> <hex file>\n");
+        return 1;
+    }
+    uint8_t *ser_dev = argv[1];
+    uint8_t *hexfile_path = argv[2];
+     
+    FILE *hexfile = fopen(hexfile_path, "w+");
+        if(!hexfile){
+            fprintf(stderr, "Error: %s\n",strerror(errno));
+            return 1;
+        }
+    fprintf(hexfile, "test1");
+    fclose(hexfile);
+
     return 0;
 }
