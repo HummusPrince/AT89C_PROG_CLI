26e6682296ba7c76f0b81350592f7b1b682b9685
HummusPrince <HummusPrince@dev.null>
2022-04-09 13:50:26 +0300

    Open tty properly

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <stdint.h>
 #include <errno.h>
-//#include <unistd.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*void config_port(*termios){
+    
+}*/
 
 //Call - at89prog <serial device> <hex file>
 //For now only device read is supported...
@@ -18,10 +24,28 @@ int main(int argc, char **argv){
     FILE *hexfile = fopen(hexfile_path, "w+");
         if(!hexfile){
             fprintf(stderr, "Error: %s\n",strerror(errno));
-            return 1;
+            return 2;
         }
-    fprintf(hexfile, "test1");
-    fclose(hexfile);
+
+    int serial_port_fd  = open(ser_dev, O_RDWR);
+    if(serial_port_fd < 0){
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+        return 3;
+    }
+
+    struct termios tty;
+
+    if(tcgetattr(serial_port_fd, &tty)){
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+        return 4;
+    }
+    
+    close(serial_port_fd);
+    printf("Cool, working good!\n");
+    
+
+    //fprintf(hexfile, "test1");
+    //fclose(hexfile);
 
     return 0;
 }
